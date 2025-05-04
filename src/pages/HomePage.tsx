758b10fd38d1a9613343993e4796a7eb063c4596
Matt G <mpgraham917@gmail.com>
2025-05-04 00:39:43 -0700

    Add container layout to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,9 @@ export default function HomePage() {
   return (
     <div className="px-8 py-8">
       <h1 className="text-3xl font-bold mb-4 text-center">Image Viewer</h1>
-      <ImageUpload />
+      <div className="container mx-auto">
+        <ImageUpload />
+      </div>
     </div>
   );
 }
