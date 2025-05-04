6334f95fcad88e26bfd7a39ae2677374c624a6a7
Matt G <mpgraham917@gmail.com>
2025-05-04 16:45:34 -0700

    Remove unnecessary title

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import { ImageUpload } from "../components/ImageUpload";
 export default function HomePage() {
   return (
     <div className="px-8 py-8">
-      <h1 className="text-3xl font-bold mb-4 text-center">Image Viewer</h1>
       <div className="container mx-auto">
         <ImageUpload />
       </div>
